8e9901464cb55aef421cffca9f59a70ded973018
priyanshagrahari54-blip <priyanshagrahari54@gmail.com>
2026-09-19 17:16:51 +0530

    ZEROOS: add kernel task context and scheduler core

---
+++ kernel/scheduler.h
@@ -0,0 +1,9 @@
+#ifndef ZEROOS_SCHEDULER_H
+#define ZEROOS_SCHEDULER_H
+#include "types.h"
+int scheduler_init(void);
+void scheduler_tick(void);
+void scheduler_yield(void);
+void scheduler_start(void);
+uint64_t scheduler_ticks(void);
+#endif
