f2e14d825ce9e54d1db6e1570e729a72d980e294
priyanshagrahari54-blip <priyanshagrahari54@gmail.com>
2026-09-19 17:13:32 +0530

    ZEROOS: mature synchronization and timer IRQ abstractions

--- kernel/interrupts.h
+++ kernel/interrupts.h
@@ -1,34 +1,17 @@
 #ifndef ZEROOS_INTERRUPTS_H
 #define ZEROOS_INTERRUPTS_H
-
 #include "types.h"
 
 struct interrupt_frame {
-    uint64_t r15;
-    uint64_t r14;
-    uint64_t r13;
-    uint64_t r12;
-    uint64_t r11;
-    uint64_t r10;
-    uint64_t r9;
-    uint64_t r8;
-    uint64_t rbp;
-    uint64_t rdi;
-    uint64_t rsi;
-    uint64_t rdx;
-    uint64_t rcx;
-    uint64_t rbx;
-    uint64_t rax;
-    uint64_t vector;
-    uint64_t error_code;
-    uint64_t rip;
-    uint64_t cs;
-    uint64_t rflags;
-    uint64_t rsp;
-    uint64_t ss;
+    uint64_t r15,r14,r13,r12,r11,r10,r9,r8,rbp,rdi,rsi,rdx,rcx,rbx,rax;
+    uint64_t vector,error_code,rip,cs,rflags,rsp,ss;
 };
+
+typedef void (*irq_handler_t)(uint8_t irq, struct interrupt_frame *frame, void *context);
 
 void interrupts_init(void);
 void interrupt_dispatch(struct interrupt_frame *frame);
+int irq_register(uint8_t irq, irq_handler_t handler, void *context);
+int irq_unregister(uint8_t irq, irq_handler_t handler, void *context);
 
 #endif
