e7d5b0d4aed2227e7c1908f15a2650748458cac0
priyanshagrahari54-blip <priyanshagrahari54@gmail.com>
2026-09-19 16:35:31 +0530

    ZEROOS: add kernel/interrupts.h

---
+++ kernel/interrupts.h
@@ -0,0 +1,19 @@
+#ifndef ZEROOS_INTERRUPTS_H
+#define ZEROOS_INTERRUPTS_H
+
+#include <stdint.h>
+
+struct interrupt_frame {
+    uint64_t vector;
+    uint64_t error_code;
+    uint64_t rip;
+    uint64_t cs;
+    uint64_t rflags;
+    uint64_t rsp;
+    uint64_t ss;
+};
+
+void interrupts_init(void);
+void interrupt_dispatch(struct interrupt_frame *frame);
+
+#endif
