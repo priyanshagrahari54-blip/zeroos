4241199c2021e0b0ba66b5e5af6199416d5b8188
priyanshagrahari54-blip <priyanshagrahari54@gmail.com>
2026-09-19 19:13:56 +0530

    ZEROOS: expose scheduler timed sleep API

--- kernel/scheduler.h
+++ kernel/scheduler.h
@@ -5,6 +5,7 @@
 int scheduler_init(void);
 void scheduler_tick(void);
 void scheduler_yield(void);
+int scheduler_sleep_ticks(uint64_t ticks);
 void scheduler_start(void);
 uint64_t scheduler_ticks(void);
 
