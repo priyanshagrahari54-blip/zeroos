871312f281ad2ef50038ef2396b4739229338f84
priyanshagrahari54-blip <priyanshagrahari54@gmail.com>
2026-09-19 17:13:24 +0530

    ZEROOS: mature synchronization and timer IRQ abstractions

--- kernel/timer.h
+++ kernel/timer.h
@@ -1,10 +1,13 @@
 #ifndef ZEROOS_TIMER_H
 #define ZEROOS_TIMER_H
-
 #include "types.h"
+
+typedef void (*timer_tick_hook_t)(void);
 
 void timer_init(void);
 void timer_tick(void);
 uint64_t timer_ticks(void);
+uint32_t timer_frequency_hz(void);
+int timer_register_tick_hook(timer_tick_hook_t hook);
 
 #endif
