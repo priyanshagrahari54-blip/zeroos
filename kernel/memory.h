853270f76a5c3d4ffd64c2a3ad4356c8bd9f270b
priyanshagrahari54-blip <priyanshagrahari54@gmail.com>
2026-09-20 18:33:43 +0530

    memory: document allocator search synchronization

--- kernel/memory.h
+++ kernel/memory.h
@@ -8,6 +8,7 @@
 void memory_init(uint64_t multiboot_info);
 void *page_alloc(void);
 void *page_alloc_at(uint64_t address);
+/* IRQ-safe: serializes the bitmap search against allocator/reservation updates. */
 uint64_t memory_find_free_run(uint64_t need_pages);
 void *page_alloc_zero(void);
 void page_free(void *address);
