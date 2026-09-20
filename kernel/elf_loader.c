7b376cd55c55eab949587968c4ead1650b80eb87
priyanshagrahari54-blip <priyanshagrahari54@gmail.com>
2026-09-20 20:18:03 +0530

    Correct ELF loader overflow helper calls

--- kernel/elf_loader.c
+++ kernel/elf_loader.c
@@ -2,7 +2,7 @@
 #include "memory.h"
 #include "vmm.h"
 
-static int elf_loader_elf_loader_add_overflow_u64(uint64_t a, uint64_t b, uint64_t *out) {
+static int elf_loader_add_overflow_u64(uint64_t a, uint64_t b, uint64_t *out) {
     if (b > ~0ULL - a) return 1;
     *out = a + b;
     return 0;
